cb911028db01d350452f653afeedd83e0242e829
Mattia PRIMAVERA <mprimavera@aedmap.com>
2017-08-04 17:08:36 +0200

    [Spinner] Material design Spinner Form widget

--- src/main/java/com/mprimavera/pearform/model/fields/Spinner.java
+++ src/main/java/com/mprimavera/pearform/model/fields/Spinner.java
@@ -4,13 +4,14 @@ import android.content.Context;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.util.AttributeSet;
-
+import android.widget.ArrayAdapter;
 import com.mprimavera.pearform.contracts.IValidator;
 import com.mprimavera.pearform.R;
 import com.mprimavera.pearform.model.FieldWidget;
+import com.weiwangcn.betterspinner.library.material.MaterialBetterSpinner;
 
 public class Spinner extends FieldWidget {
-    private android.widget.Spinner mSpinner;
+    private MaterialBetterSpinner mSpinner;
     private IFieldValidator mValidator;
 
     public Spinner(Context context) {
@@ -35,7 +36,43 @@ public class Spinner extends FieldWidget {
 
     public void init() {
         inflate(getContext(), R.layout.form_input_spinner_field, this);
-        mSpinner = findViewById(R.id.spinner);
+        mSpinner = findViewById(R.id.materialSpinner);
+    }
+
+    public Spinner hintColor(int color) {
+        mSpinner.setHintTextColor(color); return this;
+    }
+
+    public Spinner hint(int hint) {
+        mSpinner.setHint(hint);
+        return this;
+    }
+
+    public Spinner hint(String hint) {
+        mSpinner.setHint(hint);
+        return this;
+    }
+
+    public Spinner floatingHint(String hint) {
+        mSpinner.setFloatingLabelText(hint);
+        return this;
+    }
+
+    public Spinner floatingHint(String hint, int color) {
+        mSpinner.setFloatingLabelText(hint);
+        mSpinner.setFloatingLabelTextColor(color);
+        return this;
+    }
+
+    public Spinner validateWith(IValidator validator) {
+        mValidator = (IFieldValidator) validator;
+        return this;
+    }
+
+    public Spinner elements(String[] elements) {
+        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<>(getContext(), android.R.layout.simple_dropdown_item_1line, elements);
+        mSpinner.setAdapter(arrayAdapter);
+        return this;
     }
 
     @Override public boolean validate() {
@@ -45,7 +82,7 @@ public class Spinner extends FieldWidget {
     @Override
     public Bundle getValue() {
         Bundle bundle = new Bundle();
-        bundle.putString(mResultKey, Integer.toString(mSpinner.getSelectedItemPosition()));
+//        bundle.putString(mResultKey, Integer.toString(mSpinner.()));
         return bundle;
     }
 
