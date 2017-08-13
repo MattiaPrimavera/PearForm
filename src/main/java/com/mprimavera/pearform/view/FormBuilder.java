32e6890704ba673b94ac7dcf60c4b55b502e23f4
Mattia PRIMAVERA <mprimavera@aedmap.com>
2017-08-13 04:21:36 +0200

    [FormBuilder] adding text, MaterialText and switcher constructors

--- src/main/java/com/mprimavera/pearform/view/FormBuilder.java
+++ src/main/java/com/mprimavera/pearform/view/FormBuilder.java
@@ -1,8 +1,10 @@
 package com.mprimavera.pearform.view;
 
 import android.content.Context;
+import android.widget.Gallery;
 
 import com.mprimavera.pearform.model.fields.Spinner;
+import com.mprimavera.pearform.model.fields.Switch;
 import com.mprimavera.pearform.model.fields.material.MaterialText;
 
 public class FormBuilder {
@@ -22,6 +24,13 @@ public class FormBuilder {
         return text;
     }
 
+    public MaterialText text(String resultBundleKey, String hint, int iconResource) {
+        MaterialText text = new MaterialText(mContext).init(hint);
+        text.setResultKey(resultBundleKey);
+        text.setIconResource(iconResource);
+        return text;
+    }
+
     public MaterialText text(String resultBundleKey, String hint, MaterialText.IFieldValidator fieldValidator) {
         MaterialText text = new MaterialText(mContext).init(hint);
         text.setResultKey(resultBundleKey);
@@ -29,9 +38,24 @@ public class FormBuilder {
         return text;
     }
 
+    public MaterialText text(String resultBundleKey, String hint, int iconResource, MaterialText.IFieldValidator fieldValidator) {
+        MaterialText text = new MaterialText(mContext).init(hint);
+        text.setResultKey(resultBundleKey);
+        text.setValidator(fieldValidator);
+        text.setIconResource(iconResource);
+        return text;
+    }
+
     public Spinner spinner(String resultBundleKey) {
         Spinner spinner = new Spinner(mContext);
         spinner.setResultKey(resultBundleKey);
         return spinner;
+    }
+
+    public Switch switcher(String resultBundleKey, String text, int iconResource) {
+        Switch switcher = new Switch(mContext).init(text);
+        switcher.setResultKey(resultBundleKey);
+        switcher.setIconResource(iconResource);
+        return switcher;
     }
 }
